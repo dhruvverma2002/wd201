efb0c8c7f1b29b033df9783282fe0c0598318d66
Dhruv Verma <dhruv.verma8@gmail.com>
2023-04-05 23:56:25 +0530

    level 7 work in progress

--- todo-app/models/todo.js
+++ todo-app/models/todo.js
@@ -7,12 +7,16 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
-      // define association here
-    }
+    // static associate(models) {
+    //   // define association here
+    // }
 
     static addTodo({ title, dueDate }) {
       return this.create({ title: title, dueDate: dueDate, completed: false });
+    }
+
+    static getTodos() {
+      return this.findAll();
     }
 
     markAsCompleted() {
